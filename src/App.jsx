e00328ec9c29f49de3ca9ee284f409a23313af1e
Husoschi Ionut-Catalin <ionuthusoschi@gmail.com>
2023-11-09 09:21:58 +0200

    test

---
+++ src/App.jsx
@@ -0,0 +1,251 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster: "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+const KEY = "20336a5e";
+
+export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  useEffect(function () {
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`http://www.omdbapi1.com/?apikey=${KEY}&s=interstellar`);
+        if (!res.ok) throw new Error("Something went wrong!");
+        const data = await res.json();
+        setMovies(data.Search);
+        setIsLoading(false);
+      } catch (error) {
+        setMessage(error.message);
+      }
+    }
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <Navbar>
+        <SearchMoviesInput />
+        <MoviesNumber numMovies={movies.length} />
+      </Navbar>
+      <MainSection>
+        <Box>{isLoading ? <Loader /> : <MoviesList movies={movies} />}</Box>
+        <Box>
+          <SummaryList watched={watched} />
+          <WatchedList watched={watched} />
+        </Box>
+      </MainSection>
+    </>
+  );
+}
+
+const Loader = () => {
+  return (
+    <>
+      <p className="loader">Is loading...</p>
+    </>
+  );
+};
+
+const Navbar = ({ children }) => {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+};
+
+const SearchMoviesInput = () => {
+  const [query, setQuery] = useState("");
+  return (
+    <>
+      <input className="search" type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)} />
+    </>
+  );
+};
+
+const MoviesNumber = ({ numMovies }) => {
+  return (
+    <>
+      <p className="num-results">
+        Found <strong>{numMovies}</strong> results
+      </p>
+    </>
+  );
+};
+
+const MainSection = ({ children }) => {
+  return (
+    <>
+      <main className="main">{children}</main>
+    </>
+  );
+};
+
+const Box = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <>
+      <div className="box">
+        <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+          {isOpen ? "–" : "+"}
+        </button>
+        {isOpen && children}
+      </div>
+    </>
+  );
+};
+
+const MoviesList = ({ movies }) => {
+  return (
+    <>
+      <ul className="list">
+        {movies?.map((movie) => (
+          <Movie movie={movie} key={movie.imdbID} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const Movie = ({ movie }) => {
+  return (
+    <>
+      <li>
+        <img src={movie.Poster} alt={`${movie.Title} poster`} />
+        <h3>{movie.Title}</h3>
+        <div>
+          <p>
+            <span>🗓</span>
+            <span>{movie.Year}</span>
+          </p>
+        </div>
+      </li>
+    </>
+  );
+};
+
+const SummaryList = ({ watched }) => {
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  return (
+    <>
+      <div className="summary">
+        <h2>Movies you watched</h2>
+        <div>
+          <p>
+            <span>#️⃣</span>
+            <span>{watched.length} movies</span>
+          </p>
+          <p>
+            <span>⭐️</span>
+            <span>{avgImdbRating}</span>
+          </p>
+          <p>
+            <span>🌟</span>
+            <span>{avgUserRating}</span>
+          </p>
+          <p>
+            <span>⏳</span>
+            <span>{avgRuntime} min</span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const WatchedList = ({ watched }) => {
+  return (
+    <>
+      <ul className="list">
+        {watched.map((movie) => (
+          <WatchedMovie movie={movie} key={movie.imdbID} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const WatchedMovie = ({ movie }) => {
+  return (
+    <>
+      <li>
+        <img src={movie.Poster} alt={`${movie.Title} poster`} />
+        <h3>{movie.Title}</h3>
+        <div>
+          <p>
+            <span>⭐️</span>
+            <span>{movie.imdbRating}</span>
+          </p>
+          <p>
+            <span>🌟</span>
+            <span>{movie.userRating}</span>
+          </p>
+          <p>
+            <span>⏳</span>
+            <span>{movie.runtime} min</span>
+          </p>
+        </div>
+      </li>
+    </>
+  );
+};
